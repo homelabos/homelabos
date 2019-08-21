7ab3f632e5ce60660b681017f587e223b7bfe1fd
Nick Busey <nickabusey@gmail.com>
2019-08-21 09:00:00 -0600

    install_homelabos.sh

--- save_setting.sh
+++
@@ -1,5 +0,0 @@
-echo "Old setting value: "
-docker run -it -v ${PWD}:/workdir mikefarah/yq yq r settings/config.yml ${SETTING}
-docker run -it -v ${PWD}:/workdir mikefarah/yq yq w -i settings/config.yml ${SETTING}
-echo "New setting value: "
-docker run -it -v ${PWD}:/workdir mikefarah/yq yq r settings/config.yml ${SETTING}
