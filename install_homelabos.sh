28c6dbecab9fd49e41862d02a76abce1fe484cb7
Nick Busey <nickabusey@gmail.com>
2020-04-08 14:10:15 -0600

    Install script fix

--- install_homelabos.sh
+++ install_homelabos.sh
@@ -2,11 +2,22 @@
 
 VERSION=dev
 
-# Install make
 printf "\x1B[01;93m========== Updating system ==========\n\x1B[0m"
 sudo apt update
+
 printf "\x1B[01;93m========== Install make and docker ==========\n\x1B[0m"
 sudo apt install -y make docker.io
+
+printf "\x1B[01;93m========== Ensure keys exist ==========\n\x1B[0m"
+# Create .ssh/ if it doesn't exist
+[ -d ~/.ssh/ ] || mkdir ~/.ssh
+# Generate passwordless keys if they don't exist
+[ -f ~/.ssh/id_rsa ] || ssh-keygen -N "" -f ~/.ssh/id_rsa
+# Create an authorized_keys file if it doesn't exist
+[ -f ~/.ssh/authorized_keys ] || touch ~/.ssh/authorized_keys
+# Add our key to it if it is not present
+KEY=$(cat ~/.ssh/id_rsa.pub)
+grep -Fxq "$KEY" ~/.ssh/authorized_keys || cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 # Download and extract HomelabOS
 printf "\x1B[01;93m========== Download and extract HomelabOS ==========\n\x1B[0m"
