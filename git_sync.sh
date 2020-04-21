75ed1d28401c0aaba151070bc54ac82f4abdbfe3
Nick Busey <nickabusey@gmail.com>
2020-04-21 15:34:09 -0600

    Adding pre-commit hook to check if vault is encrypted. Closes #355

--- git_sync.sh
+++ git_sync.sh
@@ -1,6 +1,8 @@
 mkdir -p settings > /dev/null 2>&1
 # If there is a git repo, then attempt to update
 if [ -d settings/.git/ ]; then
+  mkdir -p settings/.git/hooks/ > /dev/null 2>&1
+  cp git_sync_pre_commit settings/.git/hooks/pre-commit
   cd settings
   echo "[38;5;208mGit Sync: [0m"
   git pull
