36450ada98ff0f788797d78ed40b139758c34f7c
Daniel Macedo <admacedo@gmail.com>
2020-04-22 01:53:19 +0100

    git hooks need to be executable

--- git_sync.sh
+++ git_sync.sh
@@ -3,6 +3,7 @@ mkdir -p settings > /dev/null 2>&1
 if [ -d settings/.git/ ]; then
   mkdir -p settings/.git/hooks/ > /dev/null 2>&1
   cp git_sync_pre_commit settings/.git/hooks/pre-commit
+  chmod +x settings/.git/hooks/pre-commit
   cd settings
   echo "[38;5;208mGit Sync: [0m"
   git pull
