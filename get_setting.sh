05b1ebfdf683a4357059f4b70284a8a733e8e78a
Gonçalo Salazar <glbsalazar@gmail.com>
2020-05-10 01:08:52 +0100

    Fixed wrong variable name

--- get_setting.sh
+++ get_setting.sh
@@ -7,7 +7,7 @@ FILE=settings/config.yml
 SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2)
 if [ -z ${SETTING_VALUE} ]; then
     FILE=settings/vault.yml
-    EXISTS_IN_VAULT=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2)
+    SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2)
     if [ -z ${SETTING_VALUE} ]; then
         echo "Key does not exist in config.yml nor vault.yml."
         # Re-encrypt vault
