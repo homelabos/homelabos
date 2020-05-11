b5e0311be7f91baa28effc9d9bb183587374aaa0
Gonçalo Salazar <glbsalazar@gmail.com>
2020-05-11 23:46:06 +0100

    Reverted changes to the get and set settings

--- set_setting.sh
+++ set_setting.sh
@@ -1,31 +1,18 @@
-#!/bin/bash
 # Decrypt vault
 make decrypt
 
 echo "Finding key: $1" 
 echo "New setting: $2"
-
-# Try to figure out where key is defined
 FILE=settings/config.yml
-SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2)
-if [ -z ${SETTING_VALUE} ]; then
-    FILE=settings/vault.yml
-    SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2)
-    if [ -z ${SETTING_VALUE} ]; then
-        echo "Key does not exist in config.yml nor vault.yml."
-        # Re-encrypt vault
-        make encrypt
-        exit 1
-    fi
-fi
+sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2 || FILE=settings/vault.yml
 
 echo "Found value in file: $FILE"
 
-echo "Old setting value: " ${SETTING_VALUE}
-# Setting the new value
+echo "Old setting value: "
+sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2
 sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq w -i $FILE $1 $2
-NEW_SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2)
-echo "New setting value: " ${NEW_SETTING_VALUE}
+echo "New setting value: "
+sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r $FILE $1 $2
 
 # Re-encrypt vault
 make encrypt
