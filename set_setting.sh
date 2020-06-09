365aeddb1798ff78a69432740c5d66a0df603164
Kim Poulsen <4174578+kpoppel@users.noreply.github.com>
2020-06-09 23:34:34 +0000

    Fix set_setting.sh script complaint about too many arguments.  Also some pre-commit lint fixes.

--- set_setting.sh
+++ set_setting.sh
@@ -8,10 +8,10 @@ echo "New setting: $2"
 # Try to figure out where key is defined
 FILE=settings/config.yml
 SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r "$FILE" "$1" "$2")
-if [ -z ${SETTING_VALUE} ]; then
+if [ -z "${SETTING_VALUE}" ]; then
     FILE=settings/vault.yml
     SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r "$FILE" "$1" "$2")
-    if [ -z ${SETTING_VALUE} ]; then
+    if [ -z "${SETTING_VALUE}" ]; then
         echo "Key does not exist in config.yml nor vault.yml."
         # Re-encrypt vault
         make encrypt
