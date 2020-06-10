d01b71e0085c3a2eeb57002f0ac3d1a30ec68366
Kevin Poorman <kjp@codefriar.com>
2020-06-10 17:54:37 -0400

    work commit

--- Tasks/ConfigTasks.sh
+++ Tasks/ConfigTasks.sh
@@ -43,4 +43,27 @@ Task::set(){
 
   Task::decrypt
 
+  # Try to figure out where key is defined
+  FILE=settings/config.yml
+  SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r "$FILE" "$1" "$2")
+  if [ -z ${SETTING_VALUE} ]; then
+      FILE=settings/vault.yml
+      SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r "$FILE" "$1" "$2")
+      if [ -z ${SETTING_VALUE} ]; then
+          echo "Key does not exist in config.yml nor vault.yml."
+          # Re-encrypt vault
+          make encrypt
+          exit 1
+      fi
+  fi
+
+  echo "Found value in file: $FILE"
+
+  echo "Old setting value: " ${SETTING_VALUE}
+  # Setting the new value
+  sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq w -i "$FILE" "$1" "$2"
+  NEW_SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir mikefarah/yq yq r "$FILE" "$1" "$2")
+  echo "New setting value: " ${NEW_SETTING_VALUE}
+
+  Task::encrypt
 }
