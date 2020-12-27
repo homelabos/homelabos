17ee721ba86c6270c57237dfb0df1a23c8a275cd
Arun Raghavan <arun@asymptotic.io>
2020-12-27 10:26:47 -0500

    Allow specifying a non-default SSH key for ansible

--- docker_helper.sh
+++ docker_helper.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 VERSION=$(cat VERSION)
+SSH_KEY=${SSH_KEY:-"${HOME}/.ssh/id_rsa"}
 
 if [ ! -f "$HOME/.homelabos_vault_pass" ]; then
     echo "Oops, I cannot find your vault password in $HOME/.homelabos_vault_pass"
@@ -8,10 +9,10 @@ if [ ! -f "$HOME/.homelabos_vault_pass" ]; then
     read -p "Press ctrl-c and create the file in the right place.  Then file a bug report."
 fi
 
-if [ -f "$HOME/.ssh/id_rsa" -a -f "$HOME/.ssh/id_rsa.pub" -a -f "$HOME/.homelabos_vault_pass" ]; then
-    docker run --rm -it \
-      -v $HOME/.ssh/id_rsa:/root/.ssh/id_rsa \
-      -v $HOME/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+if [ -f "${SSH_KEY}" -a -f "${SSH_KEY}.pub" -a -f "$HOME/.homelabos_vault_pass" ]; then
+    ./docker.sh run --rm -it \
+      -v ${SSH_KEY}:/root/.ssh/id_rsa \
+      -v ${SSH_KEY}.pub:/root/.ssh/id_rsa.pub \
       -v $(pwd):/data \
       -v $HOME/.homelabos_vault_pass:/ansible_vault_pass \
       homelabos:${VERSION} "$@"
