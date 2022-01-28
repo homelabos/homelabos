54656c2ca687806bfb8dda80fd777dc89a454f1d
Nick Busey Live <nickabusey@gmail.com>
2022-01-27 20:39:53 -0700

    New test format working

--- cmd/test.go
+++ cmd/test.go
@@ -9,9 +9,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"sort"
 	"strings"
-	"time"
 
 	"github.com/spf13/cobra"
 	"gitlab.com/nickbusey/homelabos/services"
@@ -29,9 +27,9 @@ var colorBlue string = "\033[34m"
 
 var maxTests = 5
 var testsRunning = 0
+var servicesRemaining = 0
 
-var servicesStatus = map[string]string{}
-var servicesStatusNew = map[string]string{}
+var servicesList map[string]services.Service
 
 // packageCmd represents the package command
 var testCmd = &cobra.Command{
@@ -39,7 +37,9 @@ var testCmd = &cobra.Command{
 	Short: "Test HomelabOS services",
 	Long:  `This command tests every HomelabOS service.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		services.GenerateServicesList()
+		servicesList = services.GenerateServicesList()
+		servicesRemaining = len(servicesList)
+
 		watchdog()
 
 		// Generate group_vars/all based on service list
@@ -62,45 +62,39 @@ func init() {
 }
 
 func watchdog() {
-	// Check if all services are done
-	if true {
-		servicesStatus = servicesStatusNew
 
-		keys := make([]string, 0, len(servicesStatusNew))
-		for k := range servicesStatusNew {
-			keys = append(keys, k)
-		}
-		sort.Strings(keys)
-
-		// Output test statuses
-		for _, serviceName := range keys {
-			status := servicesStatus[serviceName]
-			fmt.Print(status)
-
-			// Check if more tests can be started
-			if testsRunning < maxTests {
-				// Start the next test
-				go testService(serviceName)
+	// Check if more tests can be started
+	for _, service := range servicesList {
+		if testsRunning < maxTests {
+			status := service.Status
+			if status == "_" {
+				service.Status = "0"
+				servicesList[service.Name] = service
 				testsRunning++
-			}
+				servicesRemaining--
 
+				// Start the next test
+				go testService(service)
+			}
 		}
 
-		// Sleep and retry
-		time.Sleep(time.Second)
-		fmt.Println("\033[H\033[2J")
+	}
+
+	if servicesRemaining > 0 {
+		// Restart the watchdog
 		watchdog()
 	}
+
 }
 
-func testService(serviceName string) {
+func testService(service services.Service) {
 	serviceOk := true
 
 	// Detect if the service has a doc file 1
-	if _, err := os.Stat("./docs_software/" + serviceName + ".md"); errors.Is(err, os.ErrNotExist) {
+	if _, err := os.Stat("./docs_software/" + service.Name + ".md"); errors.Is(err, os.ErrNotExist) {
 		serviceOk = false
 	}
-	servicesStatusNew[serviceName] = "1"
+	service.Status = "1"
 
 	// Detect if the service is included in docs/index.md 2
 	buffer, err := ioutil.ReadFile("docs/index.md")
@@ -109,13 +103,13 @@ func testService(serviceName string) {
 	}
 	fileContents := string(buffer)
 
-	if !strings.Contains(fileContents, serviceName) {
+	if !strings.Contains(fileContents, service.Name) {
 		serviceOk = false
 	}
-	servicesStatusNew[serviceName] = "2"
+	service.Status = "2"
 
 	// Detect if the service is using the new include style 3
-	buffer, err = ioutil.ReadFile("roles/" + serviceName + "/tasks/main.yml")
+	buffer, err = ioutil.ReadFile("roles/" + service.Name + "/tasks/main.yml")
 	if err != nil {
 		// File doesn't exist
 		serviceOk = false
@@ -125,39 +119,52 @@ func testService(serviceName string) {
 	if !strings.Contains(fileContents, "includes/start.yml") {
 		serviceOk = false
 	}
-	servicesStatusNew[serviceName] = "3"
+	service.Status = "3"
 
+	// Output service status
+	if serviceOk {
+		fmt.Print(string(colorGreen), ".")
+		happyServices++
+	} else {
+		fmt.Print(string(colorRed), "X")
+		sadServices++
+	}
+
+	testsRunning--
+}
+
+func deployTest(service services.Service) {
 	// Test service deploy
 
 	// Enable the service 4
-	cmd := exec.Command("./set_setting.sh", serviceName+".enable", "true")
+	cmd := exec.Command("./set_setting.sh", service.Name+".enable", "true")
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
-		fmt.Println("Failed enabling " + serviceName)
+		fmt.Println("Failed enabling " + service.Name)
 	}
-	servicesStatusNew[serviceName] = "4"
+	service.Status = "4"
 
 	// Deploy the service 5
-	cmd = exec.Command("make", "update_one", serviceName)
+	cmd = exec.Command("make", "update_one", service.Name)
 
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-		fmt.Println("Failed update_one " + serviceName)
+		fmt.Println("Failed update_one " + service.Name)
 	}
-	servicesStatusNew[serviceName] = "5"
+	service.Status = "5"
 
 	// Try and hit service, get valid 200, if not wait 10 seconds try again (timeout 5mins) 6
 	ii := 0
 	success := false
 	for ii < 30 && !success {
-		resp, err := http.Get("http://" + serviceName + ".164.90.159.227.sslip.io")
+		resp, err := http.Get("http://" + service.Name + ".164.90.159.227.sslip.io")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -168,22 +175,22 @@ func testService(serviceName string) {
 			ii++
 		}
 	}
-	servicesStatusNew[serviceName] = "6"
+	service.Status = "6"
 
 	// Get screenshot of service (selenium) 7
-	cmd = exec.Command("docker", "run", "--rm", "-v", "/Users/nick/Code/HomelabOS:/srv", "lifenz/docker-screenshot", "http://"+serviceName+".164.90.159.227.sslip.io", "roles/"+serviceName+"/files/"+serviceName+".png", "800px", "5000", "1")
+	cmd = exec.Command("docker", "run", "--rm", "-v", "/Users/nick/Code/HomelabOS:/srv", "lifenz/docker-screenshot", "http://"+service.Name+".164.90.159.227.sslip.io", "roles/"+service.Name+"/files/"+service.Name+".png", "800px", "5000", "1")
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 	err = cmd.Run()
 	if err != nil {
 		// fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-		fmt.Println("Failed getting screenshot for " + serviceName)
+		fmt.Println("Failed getting screenshot for " + service.Name)
 		// return
 	}
-	servicesStatusNew[serviceName] = "7"
+	service.Status = "7"
 
 	// Disable the service 8
-	cmd = exec.Command("./set_setting.sh", serviceName+".enable", "false")
+	cmd = exec.Command("./set_setting.sh", service.Name+".enable", "false")
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 	err = cmd.Run()
@@ -191,20 +198,11 @@ func testService(serviceName string) {
 		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
 		return
 	}
-	servicesStatusNew[serviceName] = "8"
+	service.Status = "8"
 
 	// Put Test Score into service doc
 
 	// Put screenshot into service doc
 
 	// If test is perfect, add to verified service list
-
-	// Output service status
-	if serviceOk {
-		fmt.Print(string(colorGreen), ".")
-		happyServices++
-	} else {
-		fmt.Print(string(colorRed), "X")
-		sadServices++
-	}
 }
--- services/services.go
+++ services/services.go
@@ -8,17 +8,18 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-var services []Service
-var servicesStatus = map[string]string{}
-var servicesStatusNew = map[string]string{}
+var services map[string]Service
 
 type Service struct {
 	Name              string
 	Version           string
 	AdditionalConfigs string
+	// Either _ for pending or a number indicating what step it's on
+	Status string
 }
 
-func GenerateServicesList() []Service {
+func GenerateServicesList() map[string]Service {
+	services = make(map[string]Service)
 	files, err := ioutil.ReadDir("./roles")
 	if err != nil {
 		log.Fatal(err)
@@ -29,7 +30,7 @@ func GenerateServicesList() []Service {
 		// create and start new progress bar
 
 		var serviceName = file.Name()
-		servicesStatusNew[serviceName] = "_"
+
 		// Filter out HomelabOS internals
 		if !strings.Contains(serviceName, "homelabos") &&
 			serviceName != "tor" {
@@ -39,7 +40,7 @@ func GenerateServicesList() []Service {
 			yfile, err := ioutil.ReadFile("./roles/" + serviceName + "/service.yml")
 
 			if err != nil {
-				services = append(services, Service{serviceName, "latest", ""})
+				services[serviceName] = Service{serviceName, "latest", "", "_"}
 				continue
 			}
 			data := make(map[interface{}]interface{})
@@ -55,7 +56,7 @@ func GenerateServicesList() []Service {
 			}
 			for key, value := range data {
 				if key == "version" {
-					services = append(services, Service{serviceName, value.(string), additionalConfigsString})
+					services[serviceName] = Service{serviceName, value.(string), additionalConfigsString, "_"}
 				}
 			}
 		}
