a158a61cb56525cb262dbfa6de14b02c9ee85d23
julowe <julowe@gmail.com>
2023-03-23 21:57:28 -0700

    use make command & quotes in get_setting.sh as in set_setting.sh

--- get_setting.sh
+++ get_setting.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # Decrypt vault
-./docker_helper.sh ansible-vault decrypt settings/vault.yml settings/passwords/*
+make decrypt
+
+echo "Finding key: $1"
 
 # Try to figure out where key is defined
 FILE=settings/config.yml
-SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir:Z mikefarah/yq:3 yq r $FILE $1 $2)
-if [ -z ${SETTING_VALUE} ]; then
+SETTING_VALUE=$(sudo docker run --rm -v ${PWD}:/workdir:Z mikefarah/yq:3 yq r "$FILE" "$1" "$2")
+if [ -z "${SETTING_VALUE}" ]; then
     FILE=settings/vault.yml
-    SETTING_VALUE=$(sudo docker run -it --rm -v ${PWD}:/workdir:Z mikefarah/yq:3 yq r $FILE $1 $2)
-    if [ -z ${SETTING_VALUE} ]; then
+    SETTING_VALUE=$(sudo docker run --rm -v ${PWD}:/workdir:Z mikefarah/yq:3 yq r "$FILE" "$1" "$2")
+    if [ -z "${SETTING_VALUE}" ]; then
         echo "Key does not exist in config.yml nor vault.yml."
         # Re-encrypt vault
         make encrypt
         exit 1
     fi
 fi
+
+echo "Found value in file: $FILE"
 
 echo "Current setting value: " ${SETTING_VALUE}
 
