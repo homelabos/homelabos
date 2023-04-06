0a545c719ed9346470375d78920134cb6231259e
Nick Busey <nickabusey@gmail.com>
2023-04-05 20:15:11 -0600

    Docker helper fix

--- docker_helper.sh
+++ docker_helper.sh
@@ -3,12 +3,18 @@
 VERSION=$(cat VERSION)
 SSH_KEY=${SSH_KEY:-"${HOME}/.ssh/id_rsa"}
 
-if [ -f "${SSH_KEY}" -a -f "${SSH_KEY}.pub" ]; then
+if [ -f "${SSH_KEY}" -a -f "${SSH_KEY}.pub" -a -f "$HOME/.homelabos_vault_pass" ]; then
     docker run --rm -it \
       -v ${SSH_KEY}:/root/.ssh/id_rsa:Z \
       -v ${SSH_KEY}.pub:/root/.ssh/id_rsa.pub:Z \
       -v $(pwd):/data:Z \
       -v $HOME/.homelabos_vault_pass:/ansible_vault_pass:Z \
+      nickbusey/homelabos:${VERSION} "$@"
+elif [ -f "${SSH_KEY}" -a -f "${SSH_KEY}.pub" ]; then
+    docker run --rm -it \
+      -v ${SSH_KEY}:/root/.ssh/id_rsa:Z \
+      -v ${SSH_KEY}.pub:/root/.ssh/id_rsa.pub:Z \
+      -v $(pwd):/data:Z \
       nickbusey/homelabos:${VERSION} "$@"
 else
     echo "You have no SSH keys in your home directory: $HOME"
